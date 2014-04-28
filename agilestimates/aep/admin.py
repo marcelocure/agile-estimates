fbcceec368ad0544c7678e29ecf80ead8493a31a
root <root@mcure.(none)>
2014-04-27 21:48:20 -0300

    adding prototypes, relatorio parcial and changes to the system

--- agilestimates/aep/admin.py
+++ agilestimates/aep/admin.py
@@ -8,24 +8,5 @@ class customerInline(admin.StackedInline):
 class profileInline(admin.StackedInline):
     model = profile
 
-
-#class csnDetailAdmin(forms.ModelForm):
-#    class Meta:
-#        model = customer
-#        widgets = {'name': AutosizedTextarea(),
-#                   'country' : AutosizedTextarea()}
-
-
-#class csnInline(vlmModelAdmin):
-#    readonly_fields = []
-#    model = customer
-#    fields = ((('id','name','country',),) )
-#    extra = 0
-#    form = csnDetailAdmin
-    #----------------------------------------------------------------------------------
-#    def customer_link(self, obj):
-#        return mark_safe('<a href="/admin/aep/customer/%s">ediiiiit</a>' % obj.id)
-#    customer_link.short_description = 'Click to Edit'
-
 admin.site.register(customer)
 admin.site.register(profile)
