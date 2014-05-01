4a455c0384d5a069a7702d8ba55c67967c6272ff
marcelocure <marcelocure@gmail.com>
2014-05-01 11:24:26 -0300

    test commit

--- ddl.sql
+++ ddl.sql
@@ -1,2 +1,2 @@
-﻿create table aep_customer (id serial primary key, name varchar(30), country varchar(30))
-create table aep_profile  (id serial primary key, name varchar(30))
+﻿create table aep_customer (id serial primary key, name varchar(30), country varchar(30));
+create table aep_profile  (id serial primary key, name varchar(30));
